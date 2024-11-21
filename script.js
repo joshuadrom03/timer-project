7391fa2326c6a103fc327fa3c49906a466ea6395
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2024-11-21 16:35:44 +0200

    cleaned up code

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/**
-store buttons and displays in variables
-when you click the start button,the last two digits of the timer should count all the way up to 100 then reset back to 0 and repeat
-*/
-
 
 let timer = document.querySelector(".timer");
 const start = document.getElementById("start");
@@ -12,47 +7,35 @@ const message = document.querySelector(".message")
 let seconds = 0;
 let mintes = 0;
 
-    
-function startTimer(){
-
+start.addEventListener("click", function(e){
     counting = setInterval(() => {
         seconds ++;
-       
-
         if (seconds == 60){
             mintes++;
             seconds = 0;
         }
-
         timer.textContent = `${mintes}:${seconds}`;
 
     }, 1000);
-}
-
-function stopTimer(){
-    clearInterval(counting);
-    
-}
-
-
-start.addEventListener("click", function(e){
-    startTimer(60);
     message.textContent = "You have started THE TIMER!"
 })
+
+function stopTimer(){
+    clearInterval(counting); 
+}
 
 stop.addEventListener("click", function(e){
     stopTimer();
     message.textContent = "You have stopped THE TIMER!"
-
 })
 
 reset.addEventListener("click", function(e){
-
     stopTimer();
     seconds = 0;
     mintes = 0;
     timer.textContent = `${mintes}:${seconds}`;
     message.textContent = "Welcome to THE TIMER!"
+
 })
 
 
